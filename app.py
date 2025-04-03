d38e7ee238eabafb119ec1fc33d4f35c54ae1d4e
Hamza93200 <hamza.muhammad@dauphine.eu>
2025-04-03 15:40:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ import requests
 import time
 import os 
 
-os.chdir("/users/hamzamuhammad/Documents/")
-
 def black_scholes_price(option_type, S, K, T, r, sigma):
     d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
